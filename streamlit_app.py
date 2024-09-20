41608d3d2df9f63369d4ab8962e727ea54a0cce4
razamohsin321 <132683065+razamohsin321@users.noreply.github.com>
2024-09-20 07:59:43 +0000

    Graph1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-st.title('Hello World')
+st.title('Geochemistry')
 
 st.write('*Data Science Course 2024*')
 
@@ -10,9 +10,18 @@ df = pd.read_csv('data/Bastar Craton.csv')
 
 cat_names = df.columns.tolist()[27:]
 
-el1 = st.selectbox('select element' , cat_names =)
+col1, col2 = st.columns(2)
 
-st.write(el1)
+with col1:
+    el1 = st.selectbox('Select Element 1' , cat_names)
+    el2 = st.selectbox('Select Element 2' , cat_names)
+
+with col2: 
+    fig = plt.figure()
+    plt.scatter(df[el1]/10000, (df[el2]/1000))
+    st.pyplot(fig)
 
 st.dataframe(df)
+
+    
 
