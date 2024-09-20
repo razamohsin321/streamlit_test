4ecc2d29d9d9457f6f231e7571efbc5199c18350
razamohsin321 <132683065+razamohsin321@users.noreply.github.com>
2024-09-20 07:18:54 +0000

    Streamlit_Day2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
 
 st.title('Hello World')
 
 st.write('*Data Science Course 2024*')
+
+df = pd.read_csv('data/Bastar Craton.csv')
+
+cat_names = df.columns.tolist()[27:]
+
+el1 = st.selectbox('select element' ,[1, 2, 3])
+
+st.write(el1)
+
+st.dataframe(df)
+
